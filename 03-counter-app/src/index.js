3c466828ece85e0b0fa4427560903322598ff93b
Jair Yara <jair.yara11@gmail.com>
2024-07-18 14:34:00 -0500

    Add changes

--- 03-counter-app/src/index.js
+++ 03-counter-app/src/index.js
@@ -1,11 +1,11 @@
 import React from 'react';
 import ReactDOM from 'react-dom';
 import PrimeraApp from './PrimeraApp';
-// import CounterApp from './CounterApp'
+import CounterApp from './CounterApp'
 
 import './index.css'
 
 const divRoot = document.querySelector('#root');
 
-// ReactDOM.render(<CounterApp />, divRoot);
-ReactDOM.render(<PrimeraApp saludo='Hola, soy Goku' />, divRoot);
+ReactDOM.render(<CounterApp />, divRoot);
+//ReactDOM.render(<PrimeraApp saludo='Hola, soy Goku' />, divRoot);
